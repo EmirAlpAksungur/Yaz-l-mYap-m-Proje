2fcc52771f102809107bda4a2a0be56ee8d1c8a6
EmirAlpAksungur <77407633+EmirAlpAksungur@users.noreply.github.com>
2021-05-17 17:33:36 +0300

    Satici_Alici_Admin_Panel

--- ShopApp.js
+++ ShopApp.js
@@ -27,6 +27,17 @@ $(document).ready(function(){
     var kullanicilar = [
 
     ]
+    function Urun(urunNo,urunAd,urunAdet,urunFiyat,urunOnay,urunEkleyenId){
+        this.urunNo = urunNo,
+        this.urunAd = urunAd,
+        this.urunAdet = urunAdet,
+        this.urunFiyat = urunFiyat,
+        this.urunOnay = urunOnay,
+        this.urunEkleyenId = urunEkleyenId
+    }
+    var urunler = [
+
+    ]
     var sayac = 0;
     $('#btn_HesabimVar').click(function(){
         //Kullanıcı Giriş ekranına Yönlendirme
@@ -117,6 +128,9 @@ $(document).ready(function(){
         kullanicilar.push(kullanici);
         sayac++;
     }
+
+
+
     var grsKullaniciAdi ;
     var grsSifre;
     //GİRİS
@@ -142,6 +156,8 @@ $(document).ready(function(){
         if(grsKullaniciAdi == "admin" && grsSifre == "admin"){
             $('girisYap').hide();
             $('admin').show();
+            adminPanelPBK();
+            adminUrunYazdir();
         }
         for(let i=0;i<sayac;i++){
             if(grsKullaniciAdi == kullanicilar[i].kullaniciAdi){
@@ -149,10 +165,15 @@ $(document).ready(function(){
                     if(kullanicilar[i].hesapBilgi == "alici"){
                         $('girisYap').hide();
                         $('alici').show();
+                        aliciUrunYazdir();
+                        saticiEkranDüzenle(i);
+                        anlikKullanici = i;
                     }
                     if(kullanicilar[i].hesapBilgi == "satici"){
                         $('girisYap').hide();
                         $('satici').show();
+                        saticiEkranDüzenle(i);
+                        anlikKullanici = i;
                     }
                     return false;
                 }
@@ -170,5 +191,141 @@ $(document).ready(function(){
         return false;
     });
 
+    //GENEL 
+    $('.Cikis').click(function(){
+        $('kayitOl').show();
+        $('alici').hide();
+        $('satici').hide();
+        $('admin').hide();
+    });
+
+    //satici ekranı
+    var saticiUrunAd;
+    var saticiUrunAdet;
+    var saticiUrunFiyat;
+    var urunSayac = 0;
+    function saticiEkranDüzenle(i){
+        $('.para').val(kullanicilar[i].bakiye);
+    }
+    $('#btn_urun_ekle').click(function(){
+        saticiUrunAd = $('#inputUrunAd').val();
+        saticiUrunAdet = $('#inputUrunMiktar').val();
+        saticiUrunFiyat = $('#inputUrunFiyat').val();
+        if(urunKontrol()){
+            urunEkle();
+        }
+        else{
+            alert(alertMesasge);
+            alertMesasge = "";
+        }
+        return false;
+    });
+    function urunKontrol(){
+        var dogruMu = true;
+        if(saticiUrunAd == ""){
+            //ad dolu mu
+            alertMesasge += "Urun ad Kısmını Boş Bırakamazsınız.\n";
+            dogruMu = false;
+        }
+        if(saticiUrunAdet == ""){
+             //soyad dolu mu
+            alertMesasge+="Urun adet Kısmını Boş Bırakamazsınız.\n";
+            dogruMu = false;
+        }
+        if(saticiUrunFiyat == ""){
+             //sifre dolu mu
+            alertMesasge+="Urun Fiyat Kısmını Boş Bırakamazsınız.\n";
+            dogruMu = false;
+        }
+        return dogruMu;
+    }
+    function urunEkle(){
+        var urun = new Urun(urunSayac,saticiUrunAd,saticiUrunAdet,saticiUrunFiyat,"Onaylanmadı",anlikKullanici);
+        urunler.push(urun);
+        console.log(urunler);
+        urunSayac++;
+    }
+    //admin panel
+    function adminUrunYazdir(){
+        $('#adminTable').find("tr:gt(0)").remove();
+        for(k=0;k<urunSayac;k++){
+            if(urunler[k].urunOnay == "Onaylanmadı")
+            $('#adminTable').append('<tr><td>'+urunler[k].urunNo+'</td><td>'+urunler[k].urunAd
+            +'</td><td>'+urunler[k].urunAdet+'</td><td>'+urunler[k].urunFiyat+'</td><td>'+urunler[k].urunOnay+'</td></tr>');
+        } 
+    }
+    
+    $('#btn_admin_onay').click(function(){
+        let urunKod = parseInt($('#adminOnayUrunId').val());
+        urunler[urunKod].urunOnay = "Onaylandı";
+        adminUrunYazdir();
+        aliciUrunYazdir();
+    });
+    var paraBekleyenKullanici = [];
+    function adminPanelPBK(){
+        $('#adminPBKTable').find("tr:gt(0)").remove();
+        for(k=0;k<paraBekleyenKullanici.length;k++){
+            $('#adminPBKTable').append('<tr><td>'+kullanicilar[paraBekleyenKullanici[k]].numara+'</td><td>'+
+            kullanicilar[paraBekleyenKullanici[k]].kullaniciAdi+'</td><td>'+kullanicilar[paraBekleyenKullanici[k]].beklenenTutar+'</td></tr>');
+        } 
+    }
+    $('#btn_admin_kullanici_onay').click(function(){
+        for(k=0;k<paraBekleyenKullanici.length;k++){
+            if(paraBekleyenKullanici[k]==$('#adminOnayKullaniciId').val()){
+                kullanicilar[paraBekleyenKullanici[k]].bakiye =parseInt(kullanicilar[paraBekleyenKullanici[k]].bakiye)+parseInt(kullanicilar[paraBekleyenKullanici[k]].beklenenTutar);
+                paraBekleyenKullanici.splice(k,1)
+                adminPanelPBK();
+            }
+        }
+
+    });
+    //alici
+    function aliciUrunYazdir(){
+        $('#aliciTable').find("tr:gt(0)").remove();
+        for(k=0;k<urunSayac;k++){
+            if(urunler[k].urunOnay == "Onaylandı")
+            $('#aliciTable').append('<tr><td>'+urunler[k].urunNo+'</td><td>'+urunler[k].urunAd
+            +'</td><td>'+urunler[k].urunAdet+'</td><td>'+urunler[k].urunFiyat+'</td></tr>');
+        } 
+    }
+    $('#ParaYatir').click(function(){
+        alert("Talebiniz Alınmıştır En Yakın Zamanda İşleminiz Gerçekleşecektir.");
+        kullanicilar[anlikKullanici].beklenenTutar = $('#yatirmakIstenenTutar').val();
+        paraBekleyenKullanici.push(anlikKullanici);
+        adminPanelPBK();//admin panel para beklyen kullanici yazdir
+        return false;
+    });
+    $('#btn_satin_al').click(function(){
+        if(adetKontrol()){
+            if(fiyatKontrol()){
+                satisiGerceklestir();
+            }
+        }
+        return false;
+    });
+    function fiyatKontrol(){
+        var toplamFiyat = 0;
+        var donulecekDeger = true;
+        toplamFiyat = parseInt(parseInt($('#aliciUrunAdet').val()))*parseInt(urunler[parseInt($('#aliciUrunId').val())].urunFiyat);
+        if(toplamFiyat > kullanicilar[anlikKullanici].bakiye){
+            donulecekDeger = false
+        }
+        return donulecekDeger;
+    }
+    function adetKontrol(){
+        var donulecekDeger = true;
+        if(parseInt($('#aliciUrunAdet').val())>urunler[parseInt($('#aliciUrunId').val())].urunAdet){
+            donulecekDeger = false;    
+        }
+        return donulecekDeger;
+    }
+    function satisiGerceklestir(){
+        urunler[parseInt($('#aliciUrunId').val())].urunAdet = parseInt(urunler[parseInt($('#aliciUrunId').val())].urunAdet)-parseInt($('#aliciUrunAdet').val())
+        kullanicilar[anlikKullanici].bakiye =parseInt(parseInt(kullanicilar[anlikKullanici].bakiye)-(parseInt(parseInt($('#aliciUrunAdet').val()))*parseInt(urunler[parseInt($('#aliciUrunId').val())].urunFiyat)));
+        saticiEkranDüzenle(anlikKullanici);
+        aliciUrunYazdir();
+        kullanicilar[urunler[parseInt($('#aliciUrunId').val())].urunEkleyenId].bakiye =
+        parseInt(parseInt(kullanicilar[urunler[parseInt($('#aliciUrunId').val())].urunEkleyenId].bakiye)+(parseInt(parseInt($('#aliciUrunAdet').val()))*parseInt(urunler[parseInt($('#aliciUrunId').val())].urunFiyat)));
+    }
 
 });
